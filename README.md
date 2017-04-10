e267a50ea089906dd04da7f6feb67e13c8349b95
Richard Evans <rickecon@gmail.com>
2017-04-09 21:31:16 -0500

    Updated README.md and hrs_by_age.py. Also added an images folder.

--- README.md
+++ README.md
@@ -1,6 +1,48 @@
-# Function to generate vector of hours worked by age from CPS data
-This repository contains the work for creating a Python function `hrs_by_age()` that takes as inputs a vector of age cutoffs, a "MMYY" numerical string beginning CPS survey month to use and a "MMYY" numerical string ending CPS survey month to use. This function returns a vector of average hours by age group that is the same size as the vector of age cutoffs.
+# Module to generate vector of hours worked by age from CPS data
+This repository contains the work for creating a Python module that contains a function `hrs_by_age()` that takes as inputs a vector of age cutoffs, a "MMYY" numerical string beginning CPS survey month to use and a "MMYY" numerical string ending CPS survey month to use. This function returns a vector of average hours by age group that is the same size as the vector of age cutoffs.
 
 The CPS data files are available on the NBER CPS data page at [http://nber.org/data/cps_basic.html](http://nber.org/data/cps_basic.html). The URL for each file and the format for a given month of the data file (compressed .zip) is `http://nber.org/cps-basic/monYYpub.zip`, where "mon" is the lower-case first three letters of a given month and "YY" are the last two digits of the four-digit year. For example, the data for the January 2017 survey compressed data file is [http://nber.org/cps-basic/jan17pub.zip](http://nber.org/cps-basic/jan17pub.zip).
 
 When the compressed file is unzipped, the resulting data file has the form "monYYpub.dat". These .dat files are fixed with text files. The dictionary for the beginning column number of each variable for data from January 2015 on is available at [http://nber.org/data/progs/cps-basic/cpsbjan2015.dct](http://nber.org/data/progs/cps-basic/cpsbjan2015.dct). Value labels for some of the varialbes for data from January 2015 on are available at [http://nber.org/data/progs/cps-basic/cpsbjan2015.do](http://nber.org/data/progs/cps-basic/cpsbjan2015.do).
+
+## Rules for calculating
+The rules for the data that are used to calculate usual weekly hours are the following. The three key variables are:
+
+* `PEHRUSL1`: "How many hours per week do you usually work at your main job?"
+  * -4: hours vary
+  * -1: n/a
+  * 0-99: weekly hours
+* `PEHRUSL2`: "How many hours per week do you usually work at your other job(s)?"
+  * -4: hours vary
+  * -1: n/a
+  * 0-99: weekly hours
+* `PEHRFTPT`: "Do you usually  work 35 hours or more per week?"
+  * 1: Yes
+  * 2: No
+  * 3: Hours vary
+
+Note that the CPS basic household survey data include a variable `PEHRUSLT` that is the sum of `PEHRUSL1` and `PEHRUSL2`. However, the variable `PEHRUSLT` excludes information when `PEHRFTPT=1` and either `PEHRUSL1` or `PEHRUSL2` equals -1 or -4. For this reason, we construct our own measure of usual weekly hours by age.
+
+The following table describes our heuristic for computing total usual weekly hours.
+
+| `PEHRUSL1` | `PEHRUSL2` | `PEHRFTPT` | `TotWklyHours` |
+|:----------:|:----------:|:----------:| --------------:|
+|      -4    |      -4    |        1   | 35.0 |
+|      -4    |      -4    |   2 or 3   | NaN |
+|      -4    |      -1    |        1   | 35.0 |
+|      -4    |      -1    |   2 or 3   | NaN |
+|      -4    |    0-99    |        1   | max(`PEHRUSL2`, 35.0) |
+|      -4    |    0-99    |   2 or 3   | `PEHRUSL2` |
+|      -1    |      -4    |        1   | 35.0 |
+|      -1    |      -4    |   2 or 3   | NaN |
+|      -1    |      -1    |        1   | 35.0 |
+|      -1    |      -1    |   2 or 3   | NaN |
+|      -1    |    0-99    |        1   | max(`PEHRUSL2`, 35.0) |
+|      -1    |    0-99    |   2 or 3   | `PEHRUSL2` |
+|    0-99    |      -4    |        1   | max(`PEHRUSL1`, 35.0) |
+|    0-99    |      -4    |   2 or 3   | `PEHRUSL1` |
+|    0-99    |      -1    |        1   | max(`PEHRUSL1`, 35.0) |
+|    0-99    |      -1    |   2 or 3   | `PEHRUSL1` |
+|    0-99    |    0-99    |        1   | max(`PEHRUSL1` + `PEHRUSL2`, 35.0 |
+|    0-99    |    0-99    |   2 or 3   | `PEHRUSL1` + `PEHRUSL2` |
+
--- hrs_by_age.py
+++ hrs_by_age.py
@@ -9,10 +9,32 @@ This Python module imports the following module(s):
 
 This Python module defines the following function(s):
     ?
+
+Permutations
+usl1    usl2    ftpt
+-4  -4  1       35
+-4  -4  other       nan
+-4  -1  1       35
+-4  -1  other       nan
+-4  0-99    1       max(usl2, 35)
+-4  0-99    other       uls2
+-1  -4  1       35
+-1  -4  other       nan
+-1  -1  1       35
+-1  -1  other       nan
+-1  0-99    1       max(usl2, 35)
+-1  0-99    other       usl2
+0-99    -4  1       max(usl1, 35)
+0-99    -4  other       usl1
+0-99    -1  1       max(usl1, 35)
+0-99    -1  other       usl1
+0-99    0-99    1       max(usl1+usl2, 35)
+0-99    0-99    other       usl1+usl2
 ------------------------------------------------------------------------
 '''
 # Import packages
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 import os
@@ -23,8 +45,114 @@ import os
 ------------------------------------------------------------------------
 '''
 
+filename = '/Users/rwe/Downloads/jan16pub.dat'
+names = ('HRHHID', 'HWHHWGT', 'PRTAGE', 'PRTFAGE', 'PEHRUSL1',
+         'PEHRUSL2', 'PEHRFTPT', 'PEHRUSLT')
+colspecs = ((0, 15), (46, 56), (121, 123), (123, 124), (217, 219),
+            (219, 221), (221, 223), (223, 226))
+df = pd.read_fwf(filename, colspecs=colspecs, header=None, names=names,
+                 index_col=False)
 
-def hrs_by_age(age_bins, beg_mmyy, end_mmyy, graph):
+# Drop all observations that:
+#   1) have no hours in either response (PEHRUSL1=-1) and (PEHRUSL2=-1)
+#   2) have [(PEHRUSL1=-1), (PEHRUSL2=-4), and (PEHRFTPT!=1)] or
+#           [(PEHRUSL1=-4), (PEHRUSL2=-1), and (PEHRFTPT!=1)]
+#   3) have age that is top-coded (PRTFAGE=1)
+df = df[((df['PEHRUSL1'] >= 0) | (df['PEHRUSL2'] >= 0) |
+        (df['PEHRFTPT'] == 1)) & (df['PRTFAGE'] == 0)]
+
+# Create empty total weekly hours series that has the index from df
+TotWklyHours = pd.Series(data=[np.nan], index=df.index)
+
+# Assume that observations that report at least 35 hours of work in the
+# typical week (PEHRFTPT=1) but report either n/a hours (-1) or varying
+# hours (-4) have a supply of 35.0 hours per week
+TotWklyHours[(df['PEHRUSL1'] < 0) & (df['PEHRUSL2'] < 0) &
+             (df['PEHRFTPT'] == 1)] = 35.0
+
+# Assume that observations that report at least 35 hours of work in the
+# typical week (PEHRFTPT=1) but report only positive hours in job 1
+# (PEHRUSL1>=0) and report n/a or varying hours in job 2 (PEHRUSL2<0)
+# have a supply of the maximum of PEHRUSL1 and 35.0
+TotWklyHours[(df['PEHRUSL1'] >= 0) & (df['PEHRUSL2'] < 0) &
+             (df['PEHRFTPT'] == 1)] = np.maximum(35.0, df['PEHRUSL1'])
+
+# Assume that observations that report at least 35 hours of work in the
+# typical week (PEHRFTPT=1) but report n/a or varying hours in job 1
+# (PEHRUSL1<0) and report only positive hours hours in job 2
+# (PEHRUSL2>=0) have a supply of the maximum of PEHRUSL2 and 35.0
+TotWklyHours[(df['PEHRUSL1'] < 0) & (df['PEHRUSL2'] >= 0) &
+             (df['PEHRFTPT'] == 1)] = np.maximum(35.0, df['PEHRUSL2'])
+
+# Observations that report only positive hours in job 1 (PEHRUSL1>=0)
+# and report n/a or varying hours in job 2 (PEHRUSL2<0) and do not
+# report at least 35 hours of work in the typical week (PEHRFTPT!=1)
+# have hours given by PEHRUSL1
+TotWklyHours[(df['PEHRUSL1'] >= 0) & (df['PEHRUSL2'] < 0) &
+             (df['PEHRFTPT'] != 1)] = df['PEHRUSL1']
+
+# Observations that report n/a or varying hours in job 1 (PEHRUSL1<0)
+# and report only positive hours in job 2 (PEHRUSL2>=0) and do not
+# report at least 35 hours of work in the typical week (PEHRFTPT!=1)
+# have hours given by PEHRUSL2
+TotWklyHours[(df['PEHRUSL1'] < 0) & (df['PEHRUSL2'] >= 0) &
+             (df['PEHRFTPT'] != 1)] = df['PEHRUSL2']
+
+# Observations that report positive hours in job 1 (PEHRUSL1>=0) and
+# positive hours in job 2 (PEHRUSL2>=0) and report at least 35 hours of
+# work in the typical week (PEHRFTPT=1) have hours given by the maximum
+# of PEHRUSL1+PEHRUSL2 and 35.0
+TotWklyHours[(df['PEHRUSL1'] >= 0) & (df['PEHRUSL2'] >= 0) &
+             (df['PEHRFTPT'] == 1)] = np.maximum(35.0, df['PEHRUSL1'] +
+                                                 df['PEHRUSL2'])
+
+# Observations that report positive hours in job 1 (PEHRUSL1>=0) and
+# positive hours in job 2 (PEHRUSL2>=0) and do not report at least 35
+# hours of work in the typical week (PEHRFTPT!=1) have hours given by
+# PEHRUSL1+PEHRUSL2
+TotWklyHours[(df['PEHRUSL1'] >= 0) & (df['PEHRUSL2'] >= 0) &
+             (df['PEHRFTPT'] != 1)] = df['PEHRUSL1'] + df['PEHRUSL2']
+
+# Add TotWklyHours to DataFrame
+df['TotWklyHours'] = TotWklyHours
+
+df_hrs_age = df.groupby('PRTAGE').apply(lambda x:
+                                        np.average(x.TotWklyHours,
+                                                   weights=x.HWHHWGT))
+
+# Create directory if images directory does not already exist
+cur_path = os.path.split(os.path.abspath(__file__))[0]
+output_fldr = 'images'
+output_dir = os.path.join(cur_path, output_fldr)
+if not os.access(output_dir, os.F_OK):
+    os.makedirs(output_dir)
+
+# Plot steady-state consumption and savings distributions
+min_age = df_hrs_age.index.min()
+max_age = df_hrs_age.index.max()
+age_pers = np.arange(min_age, max_age + 1)
+# age_pers = np.arange(1, S + 1)
+fig, ax = plt.subplots()
+plt.plot(age_pers, df_hrs_age, label='Average hours by age')
+# for the minor ticks, use no labels; default NullFormatter
+minorLocator = MultipleLocator(1)
+ax.xaxis.set_minor_locator(minorLocator)
+plt.grid(b=True, which='major', color='0.65', linestyle='-')
+plt.title('Average hours by age $s$', fontsize=20)
+plt.xlabel(r'Age $s$')
+plt.ylabel(r'Average hours')
+plt.xlim((min_age - 1, max_age + 1))
+# plt.ylim((-1.0, 1.15 * (b_ss.max())))
+plt.legend(loc='upper right')
+output_path = os.path.join(output_dir, 'hrs_by_age')
+plt.savefig(output_path)
+# plt.show()
+plt.close()
+
+
+
+def hrs_by_age(age_bins, l_tilde, beg_mmyy, end_mmyy, web=True,
+               directory=None, graph=False):
     '''
     --------------------------------------------------------------------
     Generates a vector of average hours of work by age for a given
@@ -32,9 +160,16 @@ def hrs_by_age(age_bins, beg_mmyy, end_mmyy, graph):
     surveys)
     --------------------------------------------------------------------
     INPUTS:
-    age_bins = ?
-    beg_mmyy = ?
-    end_mmyy = ?
+    age_bins = (S,) vector, beginning cutoff ages for each age bin
+    l_tilde  = scalar > 1, model time endowment for each life period
+    beg_mmyy = length 4 string, numeric two-digit month and numeric
+               last-two-digits of four-digit year for beginning month-
+               year of data period
+    end_mmyy = length 4 string, numeric two-digit month and numeric
+               last-two-digits of four-digit year for ending month-year
+               of data period
+    web      = boolean, =True if get data from NBER data website
+    dir      = string, directory of local folder where data reside
     graph    = boolean, =True if save plot of hrs_age
 
     OTHER FUNCTIONS AND FILES CALLED BY THIS FUNCTION: None
@@ -48,6 +183,14 @@ def hrs_by_age(age_bins, beg_mmyy, end_mmyy, graph):
     --------------------------------------------------------------------
     '''
     S = age_bins.shape[0]
+    beg_yr = int(beg_mmyy[-2:])
+    beg_mth = beg_mmyy[:-2]
+    end_yr = int(end_mmyy[-2:])
+    end_mth = end_mmyy[:-2]
+    file_list = ['mar16pub.dat']
+    for name in file_list:
+        filename = os.path.join(directory, name)
+
     hrs_age = age_bins
 
     if graph:
